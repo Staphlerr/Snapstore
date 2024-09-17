6cd01d7fc776e7e8874856e4080763cbf7e246d0
Staphlerr <belvaghaniabhinaya2020@gmail.com>
2024-09-17 18:20:06 +0700

    Tugas 3

--- main/views.py
+++ main/views.py
@@ -9,7 +9,7 @@ def show_main(request):
     context = {
         'name' : 'Belva Ghani Abhinaya',
         'npm': '2306203526',
-        'class': 'PBP A',
+        'class': 'A',
         'mood_entries': mood_entries
     }
 
